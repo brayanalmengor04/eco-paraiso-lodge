ab2bd6e4f5a7a8e8890a9080836a80f22978c4da
Brayan Almengor <brayanalmengor300@gmail.com>
2024-10-29 18:32:42 -0500

    config laravel + react

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Hello from React in Laravel with Vite!</h1>;
+}
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.jsx'], // Cambia esto a tu archivo principal de React
             refresh: true,
         }),
+        react(),
     ],
 });
